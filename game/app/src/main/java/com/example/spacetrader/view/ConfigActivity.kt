25e70a5136bf2468c5040fcd354b4d6efca27e9e
Ashley DelVentura <ashley@delventura.com>
2019-02-17 18:59:31 -0500

    Back button

--- game/app/src/main/java/com/example/spacetrader/view/ConfigActivity.kt
+++ game/app/src/main/java/com/example/spacetrader/view/ConfigActivity.kt
@@ -1,16 +1,17 @@
 package com.example.spacetrader.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.widget.*
 import com.example.spacetrader.R
 import com.example.spacetrader.model.Difficulty
 
 import kotlinx.android.synthetic.main.activity_config.*
+import kotlinx.android.synthetic.main.content_start.*
 
 class ConfigActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     private var difficultySpinner: Spinner? = null
@@ -25,6 +26,13 @@ class ConfigActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         val difficultyAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, Difficulty.values())
         difficultyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         difficultySpinner?.setAdapter(difficultyAdapter)
+
+        val createButton = findViewById(R.id.createButton) as Button
+        createButton.setOnClickListener {
+            Toast.makeText(this@ConfigActivity, "New player with name ___ and difficulty ____", Toast.LENGTH_SHORT).show()
+        }
+
+        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true);
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -35,4 +43,8 @@ class ConfigActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        onBackPressed()
+        return true
+    }
 }
