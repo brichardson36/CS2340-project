ef6d88a1f0180bc47eefdbfb9224c907d7818c16
IlaVienneau <ivienneau3@gatech.edu>
2019-02-18 18:52:44 -0500

    created entity folder

--- game/app/src/main/java/com/example/spacetrader/entity/Ship.kt
+++ game/app/src/main/java/com/example/spacetrader/entity/Ship.kt
@@ -1,4 +1,4 @@
-package com.example.spacetrader.model
+package com.example.spacetrader.entity
 
 enum class Ship {
     FLEA, GNAT, FIREFLY, MOSQUITO, BUMBLEBEE, BEETLE, HORNET, GRASSHOPPER, TERMITE, WASP
--- game/app/src/main/java/com/example/spacetrader/entity/Skill.kt
+++ game/app/src/main/java/com/example/spacetrader/entity/Skill.kt
@@ -1,4 +1,4 @@
-package com.example.spacetrader.model
+package com.example.spacetrader.entity
 
 class Skill constructor(val _name: String, var _points: Int){
 
